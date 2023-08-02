cc01f335091dfaa606607602a5c13e7ca772310c
AndresAlvarado29 <nalvaradoc1@est.ups.edu.ec>
2023-08-02 12:06:13 -0500

    cambios en el ticket

--- src/app/paginas/ticket/ticket.component.ts
+++ src/app/paginas/ticket/ticket.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
+import { Ticket } from 'src/app/domain/ticket';
+import { Vehiculo } from 'src/app/domain/vehiculo';
 
 @Component({
   selector: 'app-ticket',
@@ -8,6 +10,8 @@ import { AppComponent } from 'src/app/app.component';
   styleUrls: ['./ticket.component.scss']
 })
 export class TicketComponent {
+  ticket: Ticket = new Ticket();
+  vehiculo: Vehiculo = new Vehiculo();
   constructor(private router: Router,private app: AppComponent){}
   ngOnInit(){
     setTimeout(() => {
